786467ac7e24f399b10b879a715cfc945dd3e0a2
Mike Garcia <mike@mgarcia.org>
2018-02-17 14:20:49 +1100

    Add files via upload

---
+++ cubeTextRDP/src/main.d
@@ -0,0 +1,4 @@
+src/main.o: src/main.c ../libn64/include/rcp/vi.h \
+ ../libn64/include/libn64.h ../libn64/include/stdint.h \
+ ../libn64/include/syscall.h ../libn64/include/time.h src/rdp.c src/rdp.h \
+ src/3d.c src/3dscene.c
